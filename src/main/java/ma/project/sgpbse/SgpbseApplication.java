e96cec964fab5f9950897593c78f9af6b9235d46
ELAAANOUNI Asmae <asmaeelaanouni@gmail.com>
2026-07-02 21:02:37 +0100

    initialisation et configuration de l'environnement

---
+++ src/main/java/ma/project/sgpbse/SgpbseApplication.java
@@ -0,0 +1,13 @@
+package ma.project.sgpbse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpbseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgpbseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/project/sgpbse/SgpbseApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.project.sgpbse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpbseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
