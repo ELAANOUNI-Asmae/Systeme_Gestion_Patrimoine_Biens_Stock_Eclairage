469749aa45a43edc1caeb52cde432be4240cc4d9
ELAANOUNI-Asmae <asmaeelaanouni@example.com>
2026-07-08 01:14:56 +0100

    dernier test de pipeline jenkins

--- src/main/java/ma/project/sgpbse/SgpbseApplication.java
+++ src/main/java/ma/project/sgpbse/SgpbseApplication.java
@@ -9,6 +9,7 @@ public class SgpbseApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SgpbseApplication.class, args);
+        //Ce commentaire pour teste de pipeline jenkins
     }
 
 }
