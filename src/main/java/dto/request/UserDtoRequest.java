a1de6b96724524cd1971982b42c37e17566344b7
ELAANOUNI-Asmae <asmaeelaanouni@example.com>
2026-07-08 12:58:17 +0100

    modification du repository User, ajout du dépendance spring-validator dans pom.xml et création des Dto User (Request & Response)

---
+++ src/main/java/dto/request/UserDtoRequest.java
@@ -0,0 +1,24 @@
+package dto.request;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class UserDtoRequest {
+
+    @NotNull(message = "L'email est obligatoire !")
+    @Email private String email;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 8, max = 50, message = "Le mot de passe doit contenir entre 8 et 50 caractères")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).+$",
+            message = "Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial"
+    )
+    private String pwd;
+
+}
