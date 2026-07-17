3e69cc68de2289b483346c21d6b7a7530a111fcc
ELAANOUNI-Asmae <asmaeelaanouni@example.com>
2026-07-17 11:20:09 +0100

    réctification de structure

--- src/main/java/ma/project/sgpbse/exception/PasswordMismatchException.java
+++
@@ -1,8 +0,0 @@
-package ma.project.sgpbse.exception;
-
-public class PasswordMismatchException extends RuntimeException {
-
-    public PasswordMismatchException(String message) {
-        super(message);
-    }
-}
